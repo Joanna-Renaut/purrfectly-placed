0d8969720c95ade650ec2b494e03f6759971e086
Joanna-Renaut <joannarenaut163@gmail.com>
2023-03-22 20:37:14 +0000

    Added images to pages, added JS to make forms clear on submission and give a message

--- purrfectly-placed/main.js
+++ purrfectly-placed/main.js
@@ -7,3 +7,17 @@ window.addEventListener('scroll', () => {
         backToTop.classList.remove('show');
     }
 });
+
+// Find the form element and attach a listener for when it's submitted
+const form = document.querySelector('#my-form');
+form.addEventListener('submit', (event) => {
+    // Prevent the form from actually submitting
+    event.preventDefault();
+
+    // Show the success message
+    const successMessage = document.querySelector('#success-message');
+    successMessage.style.display = 'block';
+
+    // Clear the form fields
+    form.reset();
+});
